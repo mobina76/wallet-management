e34faed05a51c192896fa0aee0711d02d4a66089
Mobina Rajabi <mobinarajabii13766@gmail.com>
2026-08-27 22:42:40 +0330

    add wallet store and compelete state handling

--- src/app/wallet/store/wallet-store.ts
+++ src/app/wallet/store/wallet-store.ts
@@ -1,7 +1,45 @@
 import { Wallet } from '../models/wallet.model';
+import { inject, Service, signal } from '@angular/core';
+import { WalletApi } from '../services/wallet-api';
 
 interface WalletState{
   wallets: Wallet[];
   isLoading: boolean;
-  error: string | null
+  error: boolean
+}
+@Service({ autoProvided: false})
+export class WalletStore{
+  private readonly walletData = inject(WalletApi)
+  private readonly store = signal<WalletState>({
+    wallets: [],
+    isLoading:false,
+    error:false
+  })
+  loadWallets(){
+    this.store.update((currentState)=>({
+      ...currentState,
+      isLoading: true,
+      error: false
+    }));
+    this.walletData.getWallets().subscribe({
+      next: (wallets) => {
+        this.store.update((currentState) => ({
+          ...currentState,
+          wallets,
+        }));
+      },error: ()=>{
+        this.store.update((currentState)=> ({
+          ...currentState,
+          isLoading: false,
+          error: true
+        }))
+      },
+      complete: () => {
+        this.store.update((currentState) => ({
+          ...currentState,
+          isLoading: false,
+        }))
+      },
+    });
+  }
 }
